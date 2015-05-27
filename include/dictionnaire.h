81f5625a4ccd5307d400ec95c798283897e2bab2
yacinend <ndiayeyacine2004@hotmail.com>
2015-05-27 17:08:03 +0200

    indentation petite modif

--- include/dictionnaire.h
+++ include/dictionnaire.h
@@ -1,23 +1,26 @@
 #define TAILLE_MAX_CODE 12
 
 typedef struct cell {
-char * chaine;
-unsigned int code; 
-cell * suite_mm_taille;
-cell * suite_taille_plus1;
+
+	char * chaine;
+	unsigned int code; 
+	cell * suite_mm_taille;
+	cell * suite_taille_plus1;
 
 }cell;
 
 
 typedef struct cell_tab {
-char car;
-unsigned int code_car;
-cell suite_taille_plus1;
+
+	char car;
+	unsigned int code_car;
+	cell suite_taille_plus1;
+	
 } cell_tab;
 
 
 typedef cell_tab dico [259] dico;
-// case 256: taille, case 257: EOF, case 258: bit clear (reset dico)
+// case 256: taille, case 257: EOF, case 258: code clear (reset dico)
 
 void init_dico(dico d);
  
