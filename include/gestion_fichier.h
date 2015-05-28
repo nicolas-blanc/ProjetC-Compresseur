d5241b4027a54f218394841fe091c450f7a91942
Nicolas BLANC <nicolas.r.blanc@gmail.com>
2015-05-28 16:47:24 +0200

    Continuation de la gestion des fichiers // Ecrire_code à tester // manque lire_code

--- include/gestion_fichier.h
+++ include/gestion_fichier.h
@@ -1,16 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h> // Attention, rajouter -lm à la fin de la commande de compilation
 
-int init_fichier(char * nom_in, char * nom_out);
+
+int init_fichier_compression(char * nom_fin, char * nom_fout);
+
+int init_fichier_decompression(char * nom_in, char * nom_out);
 
 void close();
 
-int non_EOF();
+/*
+ * Retourne vrai (0) si le pointeur dans le fichier d'entrée est à la fin du fichier, -1 sinon
+ */
+int EOF();
 
-void ecrire_char(char * chaine, int taille);
+void ecrire_char(char * chaine);
 
 void ecrire_code(unsigned int code, int taille);
 
-char lire_char();
+/*
+ * Retourne le caractère lu dans le fichier d'entrée, ou -1 si le pointeur dans le fichier est à la fin du fichier (End_Of_File)
+ */
+int lire_char();
 
+/*
+ * Retourne le code lu dans le fichier d'entrée en fonction du nombre bit donné par la taille, ou 257 si le pointeur est à la fin du fichier (End_Of_File)
+ */
 unsigned int lire_code(int taille);
--- source/gestion_fichier.c
+++ source/gestion_fichier.c
@@ -1,41 +1,108 @@
 #include "gestion_fichier.h"
 
+#define MAX_BUFFER 26
+
 static File * fin;
 static File * fout;
 
-int init_fichier(char * nom_fin, char * nom_fout) {
-	FILE* fopen(const char* nomDuFichier, const char* modeOuverture);
+static int fdf;
 
-	char * nom_in = "../"
-	nom_in = strcat("")
+static int buffer[MAX_BUFFER];
+static int nb_buf;
 
-	fin = fopen(nom_fin,"r")
+int init_fichier_compression(char * nom_fin, char * nom_fout) {
+	fin = NULL;
+	fout = NULL;
+	fdf = -1;
+	nb_buf = 0;
+
+	int e = 0;
+
+	char * ajout_fin = "../";
+	char * ajout_fout = "../";
+
+	ajout_fin = strcat(ajout_fin,nom_fin);
+	ajout_fout = strcat(ajout_fout,nom_fout);
+
+	fin = fopen(nom_fin,"r");
+	fout = fopen(nom_fout,"wb+");
+
+	if (fin == NULL || fout == NULL)
+		e--;
+		
+	return e;
+}
+
+int init_fichier_compression_decompression(char * nom_fin, char * nom_fout) {
+	fin = NULL;
+	fout = NULL;
+	fdf = -1;
+	nb_buf = 0;
+
+	int e = 0;
+
+	char * ajout_fin = "../";
+	char * ajout_fout = "../";
+
+	ajout_fin = strcat(ajout_fin,nom_fin);
+	ajout_fout = strcat(ajout_fout,nom_fout);
+
+	fin = fopen(nom_fin,"rb");
+	fout = fopen(nom_fout,"w+");
+
+	if (fin == NULL || fout == NULL)
+		e--;
+		
+	return e;
 }
 
 void close() {
-
+	fclose(fin);
+	fclose(fout);
 }
 
-int non_EOF() {
-
+int EOF() {
+	return fdf;
 }
 
 void ecrire_char(char * chaine) {
-/*	fwrite(&c,1,NB_TO_WRITE,f);
-	fflush(f);
-	return;*/
-
+	fputs(chaine, fout);
 }
 
-void ecrire_code(unsigned int code) {
+void ecrire_code(unsigned int code, int taille) {
+	char write_buffer[2];
+	
+	int masque = 0x0001;
 
+	for (int j = 0; j < taille; j++) {
+		buffer[nb_buf+j] = masque & code;
+		masque = masque << 2;
+	}
+
+	nb_buf = nb_buf + taille;
+
+	while (nb_buf >= 15) {
+		for (int i = 0; i < 16; i++)
+			write_buffer[abs(i/8)] = write_buffer[abs(i/8)] | (buffer[i] << pow(2,i));
+
+		for (i; i < MAX_BUFFER; i++)
+			buffer[i-16] = buffer[i];
+
+		nb_buf = nb_buf - 16;
+
+		fwrite(write_buffer,sizeof(char),2,fout);
+	}
 }
 
-char lire_char() {
-/*	char res;
-	res = fread(&res,1,NB_TO_READ,f);
-	return res;
-*/
+int lire_char() {
+	int car = fgetc(fin);
+	
+	if(car == EOF) {
+		fdf = 0;
+		car = -1;
+	}
+
+	return car;
 }
 
 unsigned int lire_code(int taille) {
