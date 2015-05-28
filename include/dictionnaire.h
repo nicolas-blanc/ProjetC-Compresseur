3e252ef980c9c12925ff9572532050f166de1eac
WeHaveCookie <quentin.gerry@gmail.com>
2015-05-28 19:36:58 +0200

    Ecriture des fonctions : 	getCharByCode - Finish 	getCodeByChar - Finish 	add - Build 	getSize - Finish 	init - Finish 	reset - Finish
    Ajout des variables global :
    	Dico dico
    	TabDico hasmap
    	int sizeOfDico

--- include/dictionnaire.h
+++ include/dictionnaire.h
@@ -1,4 +1,8 @@
+#include <cmath>
+#include <string.h>
 #define TAILLE_MAX_CODE 12
+
+
 
 typedef struct cell {
 char* string;
@@ -7,25 +11,28 @@ cell* next;
 cell* down;
 } cell;
 
-
+/*
 typedef struct tab {
-char car;
-unsigned int code;
 cell* down;
 } cell_tab;
+*/
 
+typedef struct cell* TabCode[4096]
 
-typedef struct cell* tabCode[4096]
-
-typedef tab Dico[258];
+typedef cell* Dico[258];
 
 
 // case 256: taille, case 257: EOF, case 258: bit clear (reset dico)
 
 ////////////////////////////////////////////////////////////////
-// Author : Coralie
+// Author : Quentin
 //
-int get(char c);
+char* getCharByCode(unsigned int c);
+
+////////////////////////////////////////////////////////////////
+// Author : Quentin
+//
+unsigned int getCodeByChar(char* c);
 
 ////////////////////////////////////////////////////////////////
 // Author : Coralie
@@ -39,11 +46,6 @@ int getSize();
 
 ////////////////////////////////////////////////////////////////
 // Author : Quentin
-//
-void growSize();
-
-////////////////////////////////////////////////////////////////
-// Author : Quentin
 // Spe : Initialise le dictionnaire
 void init();
 
@@ -52,5 +54,12 @@ void init();
 //
 bool isExist(char c);
 
+////////////////////////////////////////////////////////////////
+// Author : Quentin
+//
+void reset();
+
 // Variable global //
-Dico* dico = init();
+Dico dico = init();
+TabDico hashmap;
+int sizeOfDico = 258;
--- source/dictionnaire.c
+++ source/dictionnaire.c
@@ -1,42 +1,94 @@
 #include ../include/dictionnaire.h
-#include <string.h>
 
 ////////////////////////////////////////////////////////////////
-int get(char * c){
-
-
+// Author : Quentin
+//
+char* getCharByCode(unsigned int c){
+    return hashmap[c].string;
 }
 
 ////////////////////////////////////////////////////////////////
-int getchaine(unsigned int c){
-
+// Author : Quentin
+//
+unsigned int getCodeByChar(char* c){
+    return isExist(c);
 }
 
-////////////////////////////////////////////////////////////////
-void add(char c){
-
-}
 
 ////////////////////////////////////////////////////////////////
-int getSize(){
+void add(char* c){
+    if (c != null){
+        if(isExist(c)<0){
+            int currentNode = 1;
+            char* researchString = malloc(sizeof(c));
+            researchString = c[0];
 
-}
+            // on recupère les cell pour le parcours
+            cell* currentCell = dico[researchString];
+            cell* previousCell = currentCell;
 
-////////////////////////////////////////////////////////////////
-void growSize(){
+            // On prepare notre nouvelle cell
+            cell* newCell = malloc(sizeof(cell*));
+            newCell->string = c;
+            newCell->code = sizeOfDico;
+            sizeOfDico++;
+            newCell->next = null;
+            newCell->down = null;
+
+            if (currentCell == null) {
+                // Il n'y aucune cellule, on rajoute directement la nouvelle cellule
+                dico[researchString] = newCell;
+            } else {
+                // Il existe deja des cellules, on cherche le bon emplacement
+                while(currentNode < strlen(c)) {
+                    strcat (researchString, c[currentNode]);
+                    currentNode++;
+                    while(strcmp(researchString,currentCell->String)>0 && currentCell != null) {
+                        previousCell = currentCell;
+                        currentCell = currentCell->next;
+                    }
+                    if (currentCell == null) {
+                        // On a trouve l'endroit ou inserer la cell. Il nous faut determiner si on l'insert en next ou en down
+                        if(strlen(c)>currentNode) {
+                            // On insert en down
+                            previousCell->down = newCell;
+                        } else {
+                            // On insert en next
+                            previousCell->next = newCell;
+                        }
+                    } else {
+                        // On verifie si la cellule courante correspond à researchString
+                        // Si c'est le cas on fait un down et on recherche plus bas
+                        // Sinon, on doit faire une insertion.
+
+                    }
+                }
+            }
+        } else {
+            // Le mot existe deja dans le dico
+        }
+
+    } else {
+        // Argument null, on ne fait rien
+    }
 
 }
 
 ////////////////////////////////////////////////////////////////
 // Author : Quentin
+int getSize(){
+    return log(sizeOfDico)/log(2)
+}
+
+////////////////////////////////////////////////////////////////
+// Author : Quentin
 void init();
+    dico = malloc(sizeof(dico));
     int i = 0;
-    for(i;i<256;i++) {
-        dico[i].car = char(i);
-        dico[i].code = i;
-        dico[i].down = null;
+    for(i;i<259;i++) {
+        //dico[i].car = (i<256)?:char(i):null;
+        dico[i] = malloc(sizeof(cell));
     }
-    dico[]
 }
 
 ////////////////////////////////////////////////////////////////
@@ -60,3 +112,12 @@ int isExist(char * c){
     }
 }
 
+////////////////////////////////////////////////////////////////
+// Author : Quentin
+void reset(){
+    int i = 0;
+    for(i;i<sizeOfDico;i++){
+        free(hashmap[i]);
+    }
+    init();
+}
